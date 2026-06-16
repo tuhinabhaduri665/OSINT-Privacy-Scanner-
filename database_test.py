8bb779eec42b9700efc24af2a1f55be56653f9cb
unknown <bhadurituhina@gmail.com>
2026-06-16 11:52:38 +0530

    Inserting multiple values

--- database_test.py
+++ database_test.py
@@ -4,9 +4,16 @@ connection = sqlite3.connect("scan_history.db")
 
 cursor = connection.cursor()
 
-cursor.execute('''SELECT * FROM scan_history
-               WHERE timestamp >= '2023-01-01' ''')
-records = cursor.fetchall()
-print("Database records: ", records)
+cursor.executemany('''INSERT INTO scan_history (username, score, risk, timestamp)
+               VALUES (?, ?, ?, ?)''',
+               [
+               ("test_user1", 75, "High", "2023-10-01"),
+               ("test_user2", 43, "Low", "2026-09-06"),
+               ("test_user3", 60, "Medium", "2024-05-20")
+               ]
+            )
+
+connection.commit()
+print("Records inserted successfully")
 
 connection.close()
